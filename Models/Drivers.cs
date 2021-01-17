c61050b3d4310e6a0fd36e80da33b0ec73b578f8
csaffold <ceciliasaffold@gmail.com>
2021-01-17 17:58:20 -0600

    Added PDSA Controller and Views

---
+++ Models/Drivers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pdsatracker.Models
+{
+    public class Drivers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Drivers()
+        {
+        }
+        public Drivers(string name)
+        {
+            Name = name;
+            Description = Description;
+        }
+
+    }
+}
